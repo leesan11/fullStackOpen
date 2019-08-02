e72e71a18cd4aa31741c4e4fbff50a14e7d04ba9
sang <leesanxi@gmail.com>
2019-08-02 18:21:00 -0400

    1.7 complete

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 
 const Header = ({heading}) => (<h1>{heading}</h1>);
 const Button = ({text, handleClick}) => (<button onClick={handleClick}>{text}</button>)
-const Result = ({good, neutral, bad}) => {
+const Result = ({good, neutral, bad, all, average, positive}) => {
     return(<>
     <p>Good: {good}</p>
     <p>Neutral: {neutral}</p>
     <p>Bad: {bad}</p>
+    <p>All: {all}</p>
+    <p>Average: {average}</p>
+    <p>Positive: {positive}</p>
     </>)
 }   
 
@@ -20,6 +23,9 @@ const [badCount, badSet] = useState(0);
 const goodClick = () => goodSet(goodCount+1)
 const neutralClick = () => neutralSet(neutralCount+1)
 const badClick = () => badSet(badCount+1)
+let all = goodCount + badCount + neutralCount
+let average = (goodCount + (-1 * badCount))/all
+let positive = goodCount*100/all + "%"
 
     return(<>
 <Header heading={"give feedback"}/>
@@ -27,7 +33,7 @@ const badClick = () => badSet(badCount+1)
 <Button text="Neutral" handleClick={neutralClick}/>
 <Button text="Bad" handleClick={badClick}/>
 <Header heading={"Statistics"}/>
-<Result good={goodCount} bad={badCount} neutral={neutralCount}/>
+<Result good={goodCount} bad={badCount} neutral={neutralCount} all={all} average={! isNaN(average)?average:0} positive={! isNaN(positive)?positive:0}/>
     </>)
 }
 
