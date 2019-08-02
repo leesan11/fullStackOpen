1635d1db2b53e49a21fb8d245c710d668bc1a6e6
sang <leesanxi@gmail.com>
2019-08-02 16:02:32 -0400

    1.2 course information completed

--- part1/src/index.js
+++ part1/src/index.js
@@ -8,18 +8,22 @@ const Header = (props) => {
     )
 }
 
+const Part = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercise}
+        </p>
+    )
+}
+
 const Content = (props) => {
-    return (<>
-        <p>
-            {props.part1} {props.exercises1}
-        </p>
-        <p>
-            {props.part2} {props.exercises2}
-        </p>
-        <p>
-            {props.part3} {props.exercises3}
-        </p>
-    </>)
+    return (
+        <div>
+            <Part part={props.part1} exercise={props.exercises1} />
+            <Part part={props.part2} exercise={props.exercises2}/>
+            <Part part={props.part3} exercise={props.exercises3}/>
+        </div>
+    )
 }
 
 const Total = (props) => {
@@ -39,7 +43,7 @@ const App = () => {
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1} part2={part2} part3={part3} 
+            <Content part1={part1} part2={part2} part3={part3}
                 exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
             <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
         </div>
