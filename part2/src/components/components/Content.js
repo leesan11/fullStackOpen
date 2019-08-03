cbdb632b30a4740b55ff48c5e04d73cd528cb04b
sang <leesanxi@gmail.com>
2019-08-03 12:39:08 -0400

    2.2 completed

--- part2/src/components/components/Content.js
+++ part2/src/components/components/Content.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import Part from './components/Part'
+import Total from './components/Total'
 
-const Content = ({parts}) =>parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises}/>)
+const Content = ({parts}) =>{
+    const getParts = () => {
+        let total = 0
+        let result = parts.map(part => {
+            total += part.exercises
+            return <Part key={part.id} name={part.name} exercises={part.exercises}/>
+            })
+        result.push(<Total key={parts.length+1} total={total}/>)
+
+        return result
+        }
+        return getParts()
+    }
 
 export default Content
---
+++ part2/src/components/components/components/Total.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Total = ({total}) => <p>Total of {total} exercises</p>
+
+export default Total
