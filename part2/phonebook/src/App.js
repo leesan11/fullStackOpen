38f6eec5c85662a4642647e017e75757801f38c5
sang <leesanxi@gmail.com>
2019-08-03 16:07:35 -0400

    completed 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,9 +8,14 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setPersons([...persons,{
-      name:newName
-    }])
+    
+    if(persons.map(person=>person.name).indexOf(newName) === -1){
+      setPersons([...persons,{
+        name:newName
+      }])
+    }else{
+      alert(`${newName} is already added to the phonebook`)
+    }    
   }
 
   const handleInput = (event) => {
