a2d6ab6f33abbc699209f39d4831b055a76691dc
sang <leesanxi@gmail.com>
2019-08-04 19:44:02 -0400

    3.7

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const phonebook = require('./db.json');
 const fs = require('fs');
+const morgan = require('morgan');
 app.use(bodyParser.json());
-
+app.use(morgan('tiny'))
 app.get("/api/persons/:id", (request, response) => {
     let result = (phonebook.persons).find(person=>person.id == request.params.id)
     if(result){
@@ -48,6 +49,8 @@ app.get("/api/persons", (request, response) =>{
 app.get("/info", (request, response) => {
     response.send(`Phonebook has info for ${phonebook.persons.length} people.<br>${new Date()}`)
 })
+
+
 
 app.listen(3001, () => {
     console.log("listening to 3001")
