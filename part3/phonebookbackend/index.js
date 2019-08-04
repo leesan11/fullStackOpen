224d75c1f80bbc4d4f88c73770fc8ddb86db6b89
sang <leesanxi@gmail.com>
2019-08-04 19:48:51 -0400

    completed 3.8

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -5,7 +5,18 @@ const phonebook = require('./db.json');
 const fs = require('fs');
 const morgan = require('morgan');
 app.use(bodyParser.json());
-app.use(morgan('tiny'))
+morgan.token('post', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.post(req,res)
+    ].join(' ')
+  }))
+
 app.get("/api/persons/:id", (request, response) => {
     let result = (phonebook.persons).find(person=>person.id == request.params.id)
     if(result){
