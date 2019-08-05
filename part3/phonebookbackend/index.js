ef155f586f1998155f0ed656d9769fe1bc5fed17
sang <leesanxi@gmail.com>
2019-08-05 10:26:45 -0400

    deploy heroku

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const phonebook = require('./db.json');
+// const phonebook = require('./db.json');
 const fs = require('fs');
 const morgan = require('morgan');
 const cors = require('cors')
@@ -21,6 +21,40 @@ app.use(morgan(function (tokens, req, res) {
     ].join(' ')
   }))
 
+let phonebook = {
+    "persons": [
+        {
+            "name": "Arto Hellas",
+            "number": "040-123456",
+            "id": 1
+        },
+        {
+            "id": 2,
+            "name": "Ada Lovelace",
+            "number": "123"
+        },
+        {
+            "name": "Dan Abramov",
+            "number": "12-43-234345",
+            "id": 3
+        },
+        {
+            "name": "Mary Poppendieck",
+            "number": "39-23-6423122",
+            "id": 4
+        },
+        {
+            "name": "asd",
+            "number": "123",
+            "id": 894
+        },
+        {
+            "name": "qwe",
+            "number": "123",
+            "id": 114
+        }
+    ]
+};
 app.get("/api/persons/:id", (request, response) => {
     let result = (phonebook.persons).find(person=>person.id == request.params.id)
     if(result){
@@ -31,29 +65,33 @@ app.get("/api/persons/:id", (request, response) => {
 })
 
 app.delete("/api/persons/:id", (request, response)=>{
-    let db = JSON.parse(fs.readFileSync(`${__dirname}/db.json`).toString());
-    let result = (db.persons).filter(person => person.id != request.params.id)    
-    fs.writeFile(`${__dirname}/db.json`, JSON.stringify({persons:result}),()=>{
-        response.status(204).json({"data":"deleted"})
-    });  
+    // let db = JSON.parse(fs.readFileSync(`${__dirname}/db.json`).toString());
+    let result = (phonebook.persons).filter(person => person.id != request.params.id)
+    phonebook.persons = result
+    response.status(204).json({"data":"deleted"})
+    // fs.writeFile(`${__dirname}/db.json`, JSON.stringify({persons:result}),()=>{
+    //     response.status(204).json({"data":"deleted"})
+    // });  
 })
 
 app.post("/api/persons", (request,response) => {
     let toAddObj = request.body
-    let db = JSON.parse(fs.readFileSync(`${__dirname}/db.json`).toString());
+    // let db = JSON.parse(fs.readFileSync(`${__dirname}/db.json`).toString());
     
     if (!toAddObj.name || !toAddObj.number){
         response.status(404).json({"error":"must supply name and number"})
-    }else if( ((db.persons).find(person=>person.name == toAddObj.name)) ){
+    }else if( ((phonebook.persons).find(person=>person.name == toAddObj.name)) ){
         response.status(404).json({"error":"name must be unique"})
     }else{
         let id = Math.floor((Math.random()*1000) + 4)
         let newPerson = {...(toAddObj), 
                     id:id}
-        let result = [...db.persons, newPerson]   
-        fs.writeFile(`${__dirname}/db.json`, JSON.stringify({persons:result}),()=>{
-            response.json(newPerson)
-        });
+        let result = [...phonebook.persons, newPerson]   
+        phonebook.persons = result
+        response.json(newPerson)
+        // fs.writeFile(`${__dirname}/db.json`, JSON.stringify({persons:result}),()=>{
+        //     response.json(newPerson)
+        // });
     }
 })
 
