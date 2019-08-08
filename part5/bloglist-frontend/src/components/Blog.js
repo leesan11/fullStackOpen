a550b2a4a9f1a45c8280079ab03262d3df6105be
sang <leesanxi@gmail.com>
2019-08-08 10:11:37 -0400

    5.6 completed

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,17 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    Title: {blog.title} Author: {blog.author} Url: {blog.url}
+import React, {useState} from 'react'
+const Blog = ({ blog }) => {
+
+  const [showInfo, setShowInfo] = useState(false)
+
+return(
+  <div style={{border:"2px solid black"}}>
+    <h5 onClick={()=>setShowInfo(!showInfo)}>Title: {blog.title} <i>Author: {blog.author}</i></h5>
+    <div style={{display:`${showInfo?'block':'none'}`}}>
+      <p>Url: {blog.url} </p>
+      <p>Likes: <button>like</button></p>
+    </div>
   </div>
 )
+}
 
 export default Blog
